9b3e46dd05ded9303884dc242e9f6e69c75d3ffa
sadeepabot <115380688+sadeepabot@users.noreply.github.com>
2022-10-08 18:27:12 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy
-global.owner = ['94714188730'] //ur number 
+global.owner = ['94'] //ur number 
 global.ownertag = '94714188730' //ur tag number
 global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
 global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
